b7de4c93a3e0c40d93b09ba472eae081b1dd0344
Shunichiro Aki <register.develop@nana4.net>
2025-01-09 12:03:47 +0900

    feat: remove cut

--- src/service/face/detect.ts
+++ src/service/face/detect.ts
@@ -3,11 +3,6 @@ import { blobToBuffer, bufferToBlob } from "@/util/dataTransformUtil";
 import env from "env-var";
 
 const FACE_DETECTOR_API = env.get("FACE_DETECTOR_API").required().asString();
-// 検出範囲を狭めるとパフォーマンスが向上する
-const DETECT_X1 = env.get("DETECT_X1").asString();
-const DETECT_Y1 = env.get("DETECT_Y1").asString();
-const DETECT_X2 = env.get("DETECT_X2").asString();
-const DETECT_Y2 = env.get("DETECT_Y2").asString();
 // 80px以上ないとRekognitionが受け付けない
 const DETECT_MIN_SIZE = env.get("DETECT_MIN_SIZE").default(80).asIntPositive();
 
@@ -16,11 +11,6 @@ export default async function detectFace(
 ): Promise<Buffer | undefined> {
   const reqBlob = bufferToBlob(imageBuffer);
   const formData = new FormData();
-
-  if (DETECT_X1) formData.append("x1", DETECT_X1);
-  if (DETECT_Y1) formData.append("y1", DETECT_Y1);
-  if (DETECT_X2) formData.append("x2", DETECT_X2);
-  if (DETECT_Y2) formData.append("y2", DETECT_Y2);
   formData.append("minSize", DETECT_MIN_SIZE.toString());
   formData.append("file", reqBlob, "image.jpg");
 
