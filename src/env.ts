16d55c2cff90997dd3e041d56277113bd7c19f84
Shunichiro Aki <register.develop@nana4.net>
2025-01-14 04:25:32 +0900

    feat: env.tsの説明を修正

--- src/env.ts
+++ src/env.ts
@@ -1,35 +1,69 @@
-import { cleanEnv, num, str } from "envalid";
+import { cleanEnv, num, port, str } from "envalid";
 
 const env = cleanEnv(process.env, {
-  // RekognitionのコレクションID
-  AWS_REKOGNITION_COLLECTION_ID: str(),
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/rekognition/command/SearchFacesByImageCommand/
-  FACE_MATCH_THRESHOLD: num({ default: undefined }),
-  // ログ出力
-  LOG_LEVEL: str({ default: "info" }),
-  // HTTPサーバーのポート
-  PORT: num({ default: 3000 }),
-  // https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect
-  FACE_DETECTOR_API: str(),
-  // 80px以上ないとRekognitionが受け付けない
-  DETECT_MIN_SIZE: num({ default: 80 }),
-  DETECT_START_X: num({ default: undefined }),
-  DETECT_START_Y: num({ default: undefined }),
-  DETECT_END_X: num({ default: undefined }),
-  DETECT_END_Y: num({ default: undefined }),
-  DETECT_CONFIDENCE: num({ default: undefined }),
-  // イベントを通知するWebhook
-  WEBHOOK: str(),
-  // RingのカメラID 指定がなければ1番目のカメラを使う
-  RING_CAMERA_ID: num({ default: undefined }),
-  // リフレッシュトークンのパス
-  REFRESH_TOKEN_PATH: str({ default: ".refreshToken" }),
-  // ビデオストリームを開始してから自動終了するまでの時間
-  DETECT_TIMEOUT: num({ default: 15000 }),
-  // Rekognition APIに渡す顔の数
-  REKOGNITION_FACE_COUNT: num({ default: 3 }),
-  // スキップする画像の回数(接続して最初の方の画像は壊れていることが多いため指定)
-  SKIP_IMAGE_BUFFER_COUNT: num({ default: 3 }),
+  AWS_REKOGNITION_COLLECTION_ID: str({ desc: "RekognitionのコレクションID" }),
+  FACE_MATCH_THRESHOLD: num({
+    desc: "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/rekognition/command/SearchFacesByImageCommand/",
+    default: undefined,
+  }),
+  LOG_LEVEL: str({ desc: "ログレベル", default: "info" }),
+  PORT: port({
+    desc: "HTTPサーバーのポート",
+    default: 3000,
+  }),
+  //
+  FACE_DETECTOR_API: str({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect",
+    example: "http://face-detector.local",
+  }),
+  DETECT_MIN_SIZE: num({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect 80px以上ないとRekognitionが受け付けない",
+    default: 80,
+  }),
+  DETECT_START_X: num({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect",
+    default: undefined,
+  }),
+  DETECT_START_Y: num({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect",
+    default: undefined,
+  }),
+  DETECT_END_X: num({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect",
+    default: undefined,
+  }),
+  DETECT_END_Y: num({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect",
+    default: undefined,
+  }),
+  DETECT_CONFIDENCE: num({
+    desc: "https://nana4rider.github.io/openapi-ui/?face-detector#/default/post_detect",
+    default: undefined,
+  }),
+  WEBHOOK: str({
+    desc: "イベントを通知するWebhook",
+    example: "http://webhool.local",
+  }),
+  RING_CAMERA_ID: num({
+    desc: "RingのカメラID 指定がなければ1番目のカメラを使う",
+    default: undefined,
+  }),
+  REFRESH_TOKEN_PATH: str({
+    desc: "リフレッシュトークンのパス",
+    default: ".refreshToken",
+  }),
+  DETECT_TIMEOUT: num({
+    desc: "ビデオストリームを開始してから自動終了するまでの時間",
+    default: 15000,
+  }),
+  REKOGNITION_FACE_COUNT: num({
+    desc: "Rekognition APIに渡す顔の数",
+    default: 3,
+  }),
+  SKIP_IMAGE_BUFFER_COUNT: num({
+    desc: "スキップする画像の回数(接続して最初の方の画像は壊れていることが多いため指定)",
+    default: 3,
+  }),
 });
 
 export default env;
