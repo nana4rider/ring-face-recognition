8efb1b6e095dd6c08c000af3645e7848dc4f0313
nana4rider <register.develop@nana4.net>
2025-03-31 07:42:41 +0900

    feat: ビルドとフォーマッターを改善

--- build.js
+++ build.js
@@ -1,18 +1,17 @@
+import tsconfigPathsPlugin from "@esbuild-plugins/tsconfig-paths";
 import { build } from "esbuild";
-import alias from "esbuild-plugin-alias";
+
+const tsconfig = "tsconfig.json";
 
 await build({
   entryPoints: ["src/index.ts"],
   outfile: "dist/index.js",
   bundle: true,
+  minify: true,
   format: "esm",
   platform: "node",
-  tsconfig: "./tsconfig.json",
-  plugins: [
-    alias({
-      "@/": "./src",
-    }),
-  ],
+  tsconfig,
+  plugins: [tsconfigPathsPlugin.default({ tsconfig })],
   banner: {
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
   },
